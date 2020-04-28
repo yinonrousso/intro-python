13b305f291b89a30c66bfc2568c55ee522dab94d
Yinonrousso <yinon@ucsb.edu>
2020-04-28 13:33:08 -0700

    added lesson1

---
+++ lessons/lesson_1/lesson1.py
@@ -0,0 +1,51 @@
+# Lesson 1
+# In this lesson we discuss basic operations, variables, if/else
+# We also go over topics of boolean operators and arithmetic operators
+
+# = -> assignment operator 
+# assigns the right hand side to the left
+
+# + -> string concatination
+# "str1" + "str2" = "str1str2"
+
+# 5/2 = 4/2 + 1/2 = 2 + 1/2 = 2 + 0.5 = 2.5
+# Integer division -> 5/2 = 2 
+
+# Convert to string -> str(parameter)
+
+# Recognize value -> type(parameter)
+
+# Comparison Operators:
+# Less than <
+# Less than or equals to <=
+# Greater than >
+# Greater than or equals to >=
+# Equals to ==
+
+# if clause -> if:
+# else if clause -> elif:
+# else clause -> else:
+
+
+age = -9
+
+if age >= 65:
+    print( "your are a senior citizen")
+    
+elif age >= 18:
+    print( "you are an adult") 
+    
+elif age >= 0: 
+    print( "you are a minor")
+else:
+    print(" sperm/egg/thought ")
+    
+    
+if age > 13:
+    print("You are bar-mitzvah'd")
+else:
+    print("You are not bar-mitzvah'd")
+        
+
+    
+    
