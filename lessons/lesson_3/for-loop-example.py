fb9ac6ae854e9e2105b47c8e51b89633fb61753f
yinonrousso <43661219+yinonrousso@users.noreply.github.com>
2020-05-06 00:36:00 -0700

    Update for-loop-example.py

--- lessons/lesson_3/for-loop-example.py
+++ lessons/lesson_3/for-loop-example.py
@@ -1,7 +1,7 @@
-# l= ["butt", "crack"]
-# #example of getting index of a list in a for loop
-# for v in range(len(l)):
-#     print(l[v])
-# #example of getting item for list in for loop
-# for i in l:
-#     print(i)
+l = ["butt", "crack"]
+#example of getting index of a list in a for loop
+for v in range(len(l)):
+    print(l[v])
+#example of getting item for list in for loop
+for i in l:
+    print(i)
