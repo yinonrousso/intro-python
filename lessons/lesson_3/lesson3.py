94f9478d3d69193c1d7dd2eb1e29c71ec1b17c2f
Yinonrousso <yinon@ucsb.edu>
2020-05-06 00:12:27 -0700

    lesson 3 on loops

---
+++ lessons/lesson_3/lesson3.py
@@ -0,0 +1,53 @@
+# Last time
+# Intro to array
+# Note in python we use lists
+# list of size 6
+#        0  1 2  3  4  5
+# ages = [13,15,2,99,104,16]
+
+
+# list slicing
+# list[inclusive:exclusive]
+# print(ages[-1])
+
+# fill list named nums with numbers 1-100
+                         # inc,exc
+# nums = [num for num in range(1,101)]
+
+#infinite loop
+# i = 0
+# while(i < 5):
+#     print(i)
+#     i = i + 1
+
+
+
+
+
+# modulus operator %
+# print(11 % 4)
+
+# num = int(input("Please enter a number: "))
+# if num % 2 == 0:
+#     print("Number is Even")
+# else:
+#     print("Number is Odd")
+
+
+
+i = 0
+while(i <= 10):
+    print(i)
+    i = i + 2
+
+i = 0
+while(i <= 10):
+    if i % 2 == 0:
+        print(i)
+    i += 1
+
+print("done with while loop")
+# 5 / 2 = 4 / 2 + 1 / 2 = 2 + 1/2 =  2 Remainder 1 
+# 7 / 2 = 6 / 2 + 1/2 = 3 + 1/2 = 3 Remainder 1
+# 11 / 4 = 8 / 4 + 3 / 4 = 2 + 3/4 = 2 Remainder 3
+
