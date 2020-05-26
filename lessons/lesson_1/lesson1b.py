936b4a48545591a3f87ef35be4503aa6a95df3e7
Yinonrousso <yinon@ucsb.edu>
2020-05-26 14:33:20 -0700

    added first lesson with evan

---
+++ lessons/lesson_1/lesson1b.py
@@ -0,0 +1,120 @@
+# In this lesson, we covered primitive data types, strings, variables, operators
+# print function, and numerical conversion between base 10, 2, and 8
+
+# This is a comment
+# 
+
+# name = "evan"
+# num = 5
+# pi = 3.14
+# print(name)
+# name = "yinon"
+# print(name)
+
+
+# name = "evan"
+# print(name)
+
+
+# name = "yinon"
+# print(name)
+
+# name="nick"
+# print(name)
+# name= "john"
+# print(name)
+
+# print(name, "is age",age)
+# name= "Yinon"
+# age= 22
+# print(name,"is age",age)
+# print("A guy named", name,"is",age, "years old")
+
+# A guy named Yinon is 22 years old
+
+# integer division //
+# a = 7 / 4
+# print(a)
+# a = 7 // 4
+# print(a)
+
+# modulus operator %
+
+# x = 5 % 2
+# print(x)
+# x = 5 % 3
+# print(x)
+# x = 9 % 4
+# print(x)
+
+# 5 / 2 = 4/2 + 1/2 = 2 + 1/2 = 2 Remainder 1
+# 5/3 = 3/3 + 2/3 = 1 + 2/3 = 1 Remainder 2
+# 9/4 = 4/4 + 4/4 + 1/4 = 2 Remainder 1
+
+# x = 33291
+# print(x % 2)
+
+# x = 24 % 12
+# print(x)
+
+# base 10
+# 0  1  2  3  4  5  6  7  8  9 
+# 10 11 12 13 14 15 16 17 18 19
+# 20 21 22 23 24 25 26 27 28 29
+
+# 9 * 10^0 = 9 * 1 = 9
+# 2 * 10^0 + 1 * 10^1 = 2 * 1 + 1 * 10 = 2 + 10 = 12
+
+#         thousands    hundreds      tens           ones
+# 1274 =   1000    +    200      +    70       +     4
+#      = (1*10^3)  +  (2 * 10^2) + (7*10^1)    + (4*10^0)
+
+# 3486 =   3000    +    400      +    80       +     6
+#      = (3*10^3)  +  (4* 10^2) + (8*10^1)    + (6*10^0)
+
+# 56924 =   50000   +   6000    +    900     +    20      +     4
+#      =   (5*10^4) + (6*10^3)  +  (9* 10^2) + (2*10^1)    + (4*10^0)
+
+# # base 8 (oct)
+# 0  1  2  3  4  5  6  7  
+# 10 11 12 13 14 15 16 17
+# 20 21 22 23 24 25 26 27
+
+# 123 = 100 + 20 + 3
+#     = (1*8^2) + (2*8^1) + (3*8^0)
+#     = 64      +  16      + 3
+#     = 83 in decimal(base 10)
+
+# 24 = 20 + 4
+#    = (2*8^1) + (4*8^0) 
+#    = 16 + 4
+#    = 20 in decimal (base 10)
+
+# 10 = 10 + 0
+#     = (1*8^1) +(0*8^0)
+#     = 8 
+
+# # base 2 (binary)
+# 0  1
+# 10 11
+# 100 101
+# 110 111
+# 1000 1001
+# 1010 1011
+# 1100 1101
+# 1110 1111
+
+# 111 = 100     +   10    +    1
+#     = (1*2^2) + (1*2^1) + (1*2^0)
+#     = 4 + 2 + 1 
+#     = 7 in decimal (base 10)
+
+# x = 2
+# y = 5
+# z = x ** y
+# print(z)
+
+# x = 3
+# y = 14
+# print(x**y)
+
